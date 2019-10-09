42439a53d94b761441d0aabe1e6933fbe8f035e2
azoladyantyi <aizydyantyi@gmail.com>
2019-10-09 15:37:12 +0200

    u made some changes on my option file add find, filter, map and use spread operator

--- src/assets/data/Options.js
+++ src/assets/data/Options.js
@@ -1,16 +1,18 @@
 
-const containerData = [
-    {title: 'Plain Cone', price: 5},
-    {title: 'Sugar Con',  price: 10},
-    {title: 'paper cup',  price: 15},
-]
+const toppings = [
+    {id: 1, name: 'Crumbed peanuts'},
+    {id: 2, name: 'Caramal dip'},
+    {id: 3, name: 'Smarties'},
+    {id: 4, name: 'Oreos'},
+    {id: 5, name: 'Astros'},
+];
 
 
 
 
-containerData.forEach(element => {
+toppings.forEach(element => {
 
     console.log(element)
     
 })
-export default containerData;
+export default toppings;
--- src/options.js
+++ src/options.js
@@ -1,59 +1,75 @@
 import React, { Component } from 'react';
+import toppings from './assets/data/Options'
 
-class topping extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {value: 'topping '};
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-  
-    handleChange(event) {
-      this.setState({value: event.target.value});
-    }
-  
-    handleSubmit(event) {
-      alert('choose your topping : ' + this.state.value);
-      event.preventDefault();
-    }
-  
-    render() {
-      return (
-        <div>
-        
-          <h5>  Choose your topping:</h5>
-            <input type="checkbox"
-               value="Crumbed peanuts"
-               checked={this.state.selectedOption === "Crumbed peanuts"}
-               onChange={this.radioChange} />Crumbed peanuts&nbsp;&nbsp;&nbsp;&nbsp;
 
-        <input type="checkbox"
-               value="Caramal dip"
-               checked={this.state.selectedOption === "Caramal dip"}
-               onChange={this.radioChange}/>Caramal dip&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+class toppingsApp extends Component {
+  constructor(props) {
 
-      <input type="checkbox"
-               value="Smarties"
-               checked={this.state.selectedOption === "Smarties"}
-               onChange={this.radioChange} />Smarties&nbsp;&nbsp;&nbsp;&nbsp;
-
-       <input type="checkbox"
-               value="Oreos"
-               checked={this.state.selectedOption === "Oreos "}
-               onChange={this.radioChange} />Oreos&nbsp;&nbsp;&nbsp;&nbsp;
-
-        <input type="checkbox"
-               value="Astros"
-               checked={this.state.selectedOption === "Astros"}
-               onChange={this.radioChange} />Astros         
-        
-      </div> 
-
-      );
-    }
+    super(props);
+    this.state = {
+      toppings,
+      selectedToppings: []
+    };
   }
-  export default topping;
+
+  manageTopping = e => {
+    const toppingId = e.target.value;
+    // look at find on JS arrays
+    const currentTopping = this.state.selectedToppings.find(
+      topping => topping.id == toppingId
+    );
+
+    if (currentTopping) {
+      // remove from the list
+      // look at filter
+      const newToppingList = this.state.selectedToppings.filter(
+        topping => topping.id != toppingId
+      );
+
+      this.setState({
+        // look at spread operator
+        selectedToppings: [...newToppingList]
+      });
+    } else {
+      // add to the list
+      const topping = this.state.toppings.find(
+        topping => topping.id == toppingId
+      );
+      this.setState({
+        selectedToppings: [...this.state.selectedToppings, topping]
+      });
+    }
+  };
+
+  render() {
+    const toppingsList = this.state.toppings.map(t => (
+      <li key={t.id}>
+        <input type="checkbox" value={t.id} onClick={this.manageTopping} />{" "}
+        {t.name}
+      </li>
+    ));
+
+    const selectedToppingsList = this.state.selectedToppings.map(t => (
+      <li key={t.id}>
+        <input type="checkbox" value={t.id} onClick={this.manageTopping} />{" "}
+        {t.name}
+      </li>
+    ));
+
+    return (
+      <div className="App">
+        <h1>Hello IceCream</h1>
+        <ul>{toppingsList}</ul>
+        <h3>Selected toppings</h3>
+        <ul>{selectedToppingsList}</ul>
+      </div>
+    );
+  }
+}
+
+
+
+  export default toppingsApp;
 
 
 
@@ -73,101 +89,3 @@ class topping extends Component {
 
 
 
-
-// import React from 'react';
-// import Select from 'react-select';
- 
-// const options = [
-//   { value: 'chocolate', label: 'Chocolate' },
-//   { value: 'strawberry', label: 'Strawberry' },
-//   { value: 'vanilla', label: 'Vanilla' },
-// ];
- 
-// class App extends React.Component {
-//   state = {
-//     selectedOption: null,
-//   };
-//   handleChange = selectedOption => {
-//     this.setState({ selectedOption });
-//     console.log(`Option selected:`, selectedOption);
-//   };
-//   render() {
-//     const { selectedOption } = this.state;
- 
-//     return (
-//       <Select
-//         value={selectedOption}
-//         onChange={this.handleChange}
-//         options={options}
-//       />
-//     );
-//   }
-// }
-// export default App;
-// // import React, { Component } from 'react';
-
-// // class Toppings extends Component {
-// //   constructor() {
-// //     super();
-    
-// //     this.state = {
-// //       showMenu: false,
-// //     };
-    
-// //     this.showMenu = this.showMenu.bind&nbsp;&nbsp;(this);
-// //     this.closeMenu = this.closeMenu.bind(this);
-// //   }
-  
-// //   showMenu(event) {
-// //     event.preventDefault();
-    
-// //     this.setState({ showMenu: true }, () => {
-// //       document.addEventListener('click', this.closeMenu);
-// //     });
-// //   }
-  
-// //   closeMenu(event) {
-    
-// //     if (!this.dropdownMenu.contains(event.target)) {
-      
-// //       this.setState({ showMenu: false }, () => {
-// //         document.removeEventListener('click', this.closeMenu);
-// //       });  
-      
-// //     }
-// //   }
-
-// //   render() {Plain Cone
-// //     return (
-// //       <div>
-// //           <h5>click the button and select the topping you like!</h5>
-// //         <button onClick={this.showMenu}>
-// //         Toppings
-// //         </button>
-        
-// //         {
-// //           this.state.showMenu
-// //             ? (
-// //               <div
-// //                 className="menu"
-// //                 ref={(element) => {
-// //                   this.dropdownMenu = element;
-// //                 }}
-// //               ><br></br>
-// //                 <button value='20'> Crumbed peanuts </button>&nbsp;&nbsp; 
-// //                 <button> Caramal dip </button>&nbsp;&nbsp; 
-// //                 <button> Chocolate Dip </button>&nbsp;&nbsp; 
-// //                 <button> Smarties </button>&nbsp;&nbsp; 
-// //                 <button> Oreos </button>&nbsp;&nbsp; 
-// //                 <button> Astros </button>
-// //               </div>
-// //             )
-// //             : (
-// //               null
-// //             )
-// //         }
-// //       </div>
-// //     );
-// //   }
-// // }
-// // export default Toppings;
